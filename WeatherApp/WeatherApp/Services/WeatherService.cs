1a4faf1f8ff9e153ff8fd180a70d5660e8c20b77
M4theusj <matheusjacob312@hotmail.com>
2024-11-30 11:56:25 -0300

    feat: deserializer e exception adicionados

--- WeatherApp/WeatherApp/Services/WeatherService.cs
+++ WeatherApp/WeatherApp/Services/WeatherService.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using WeatherApp.Models;
 
@@ -9,12 +9,47 @@ namespace WeatherApp.Services
 {
     public class WeatherService
     {
+        private HttpClient _httpClient;
         public string apiKey = "f40c0da49722fc1b0a22640e8425303d";
-        private WeatherAppMainModel weatherResponse;
+
+        public WeatherService()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://api.openweathermap.org/data/2.5/");
+        }
+
         public async Task<WeatherAppMainModel> GetWeatherResponse(string cityName)
         {
-            Uri uri = new Uri($"https://api.openweathermap.org/data/2.5/weather?q={cityName}$appid={apiKey}");
-            return weatherResponse;
+            try
+            {
+                string requestUrl = $"weather?q={cityName}&appid={apiKey}";
+
+                HttpResponseMessage response = await _httpClient.GetAsync(requestUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    Debug.WriteLine("Res:" + json);
+
+                    var weatherResponse = JsonSerializer.Deserialize<WeatherAppMainModel>(json, new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+
+                    if (weatherResponse != null)
+                    {
+                        return weatherResponse;
+                    }
+
+                    throw new Exception("Null response");
+                }
+
+                throw new Exception($"Loading error:{response.StatusCode}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Request error", ex);
+            }
         }
     }
 }
