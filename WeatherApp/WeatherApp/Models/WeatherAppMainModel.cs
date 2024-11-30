c4a2816765984c143decd464b3979997effee747
M4theusj <matheusjacob312@hotmail.com>
2024-11-30 16:06:36 -0300

    fix: Pequenas imprecisões arrumadas

--- WeatherApp/WeatherApp/Models/WeatherAppMainModel.cs
+++ WeatherApp/WeatherApp/Models/WeatherAppMainModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,5 +23,6 @@ namespace WeatherApp.Models
         public int id { get; set; }
         public string name { get; set; }
         public int cod { get; set; }
+
     }
 }
