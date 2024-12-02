e12f4fb8247b9593f32c2cde34ec64197cc3e19a
M4theusj <matheusjacob312@hotmail.com>
2024-12-02 17:10:46 -0300

    fix: Código para remoção das linhas de entry

---
+++ WeatherApp/WeatherApp/FormHandler.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Maui.Handlers;
+
+#if ANDROID
+using Android.Content.Res;
+using AColor = Android.Graphics.Color;
+#endif
+
+namespace WeatherApp.Handlers
+{
+    public static class FormHandler
+    {
+        public static void RemoveBorders()
+        {
+            EntryHandler.Mapper.AppendToMapping("CustomBackground", (handler, view) =>
+            {
+#if ANDROID
+
+                handler.PlatformView.SetBackgroundDrawable(null);
+                handler.PlatformView.SetPadding(0, 0, 0, 0);
+
+                handler.PlatformView.BackgroundTintList = ColorStateList.ValueOf(AColor.Rgb(48, 48, 48));
+#elif IOS
+                handler.PlatformView.BackgroundColor = UIKit.UIColor.Clear;
+
+                handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#endif
+            });
+        }
+    }
+}
