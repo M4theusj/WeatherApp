6c51addf16475030fe3fde20330aa287f5bd2b00
M4theusj <matheusjacob312@hotmail.com>
2024-12-02 17:08:05 -0300

    feat: Direção da tela agora é horizontal

--- WeatherApp/WeatherApp/Platforms/Android/MainActivity.cs
+++ WeatherApp/WeatherApp/Platforms/Android/MainActivity.cs
@@ -4,7 +4,10 @@ using Android.OS;
 
 namespace WeatherApp
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme",
+              MainLauncher = true,
+              ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode,
+              ScreenOrientation = ScreenOrientation.Landscape)]
     public class MainActivity : MauiAppCompatActivity
     {
     }
